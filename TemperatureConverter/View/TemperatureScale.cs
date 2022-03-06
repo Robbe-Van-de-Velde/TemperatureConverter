7e1f54f81d7b603bc549706b4202c6978dc8eb9c
Robbe Van de Velde <robbe.vandevelde@student.ucll.be>
2022-03-06 14:17:45 +0100

    kelvin-temperature-scale

--- TemperatureConverter/View/TemperatureScale.cs
+++ TemperatureConverter/View/TemperatureScale.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace View
 {
-    internal class TemperatureScale
+    public interface ITemperatureScale
     {
+        string Name { get; }
+
+        double ConvertToKelvin(double temperature);
+        double ConvertFromKelvin(double temperature);
+    }
+
+    public class KelvinTemperatureScale : ITemperatureScale
+    {
+        public string Name => "Kelvin";
+
+        public double ConvertFromKelvin(double temperature)
+        {
+            return temperature;
+        }
+
+        public double ConvertToKelvin(double temperature)
+        {
+            return temperature;
+        }
     }
 }
