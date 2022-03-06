07e94239bd6030d4a02dbb4999a0509bd369fe3c
Robbe Van de Velde <robbe.vandevelde@student.ucll.be>
2022-03-06 14:08:53 +0100

    fahrenheit-binding

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -52,4 +52,23 @@ namespace View
             return kelvin;
         }
     }
+
+    public class FahrenheitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var kelvin = (double)value;
+            var farenheit = Math.Round(((kelvin - 273.15) * 1.8) + 32, 2);
+
+            return farenheit.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var fahrenheit = double.Parse((string)value);
+            var kelvin = Math.Round(((fahrenheit - 32) / 1.8 ) + 273.15, 2);
+
+            return kelvin;
+        }
+    }
 }
