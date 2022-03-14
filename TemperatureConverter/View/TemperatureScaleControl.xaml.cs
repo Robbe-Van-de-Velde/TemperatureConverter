ecdc4ce8794fa7b1f4b481f76cad92166d7f0ad0
Robbe Van de Velde <robbe.vandevelde@student.ucll.be>
2022-03-14 12:39:46 +0100

    value

--- TemperatureConverter/View/TemperatureScaleControl.xaml.cs
+++ TemperatureConverter/View/TemperatureScaleControl.xaml.cs
@@ -39,5 +39,14 @@ namespace View
         public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(string), typeof(TemperatureScaleControl), new PropertyMetadata(""));
 
 
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Value.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(double), typeof(TemperatureScaleControl), new PropertyMetadata(0));
     }
 }
